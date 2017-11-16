5556f9599fd17ba5a0d9635c3a3e8268a666aa22
Jie <jie.he366@gmail.com>
2017-11-16 02:03:44 +0000

    Update

--- model_client.py
+++ model_client.py
@@ -1,26 +1,43 @@
 import socket
 from tools import recv_one_line, s_addr
 
+def parseText(txt):
+	print('\tParsing cmds...')
+	return 'move(5,3)\n'
+def performCmds(cmds):
+	print('\tPerforming cmds:', cmds)
+	return 'Finished\n'
+
 s = socket.socket()
 s.connect(s_addr)
 print('Connected')
 
+status = ''
+
 while 1:
 	# recv = recv_one_line(s).decode()
-	recv = s.recv(1024)
+	recv = s.recv(1024).decode()
 	if not recv:
 		break
-	print('\nModel received:',recv.decode())
-	# TODO: parse sentence
-	s.sendall(b'parsed commands\n')
 
-	confirm = s.recv(1024)
-	if not confirm:
-		break
-	print("Received confirm: {}".format(confirm.decode()))
-	s.sendall(b'got confirm, finished\n')
+	if recv.startswith('Text'):
+		recv_text = recv[4:]
+		print('\nText received:', recv_text)
+		cmds = parseText(recv_text)
+		status = 'Confirm'
+		s.sendall(cmds.encode())
 
-	if b'y' in confirm:
-		pass # perform actions
+	if recv.startswith('Confirm'):
+		if status == 'Confirm':
+			status = ''
+			recv_confirm = recv[7:]
+			print('Confirm received:', recv_confirm)
+			if recv_confirm == 'Ok':
+				result = performCmds(cmds)
+				s.sendall(result.encode())
+			else:
+				s.sendall(b'Cancelled\n')
+		else:
+			s.sendall(b'\n')
 
 s.close()
